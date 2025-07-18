42e18af9b74c20e7fbd1bee0a301a71d93b7e659
LT-Schmiddy <schmidalex1996@gmail.com>
2025-07-18 15:54:22 -0400

    updating recomp incbin

--- include/mod/recomp_incbin.h
+++ include/mod/recomp_incbin.h
@@ -1,6 +1,13 @@
 #ifndef __RECOMP_INCBIN__
 #define __RECOMP_INCBIN__
 
+
+#ifdef RECOMP_INCBIN_EXTERN_ONLY
+#define RECOMP_INCBIN(identifier, filename)           \
+    extern u8 identifier[];                           \
+    extern u8 identifier##_end[]
+
+#else
 #define RECOMP_INCBIN(identifier, filename)           \
     asm(".pushsection .rodata\n"                      \
         "\t.globl " #identifier "\n"                  \
@@ -13,5 +20,5 @@
         "\t.popsection\n");                           \
     extern u8 identifier[];                           \
     extern u8 identifier##_end[]
-
+#endif
 #endif
