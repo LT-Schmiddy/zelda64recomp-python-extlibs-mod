67dd89d6b966261bff4d80296c9d8bd5d3bfc7b1
LT-Schmiddy <schmidalex1996@gmail.com>
2025-07-22 00:24:12 -0400

    Stress-testing the lookup.

--- src/tests/test_module.py
+++ src/tests/test_module.py
@@ -1,1 +1,9 @@
 print("Hello from recomp test module");
+
+fib_holder_0 = 0
+fib_holder_1 = 1
+def fibonacci_step():
+    global fib_holder_0, fib_holder_1
+    fib_next = fib_holder_0 + fib_holder_1
+    fib_holder_0 = fib_holder_1
+    fib_holder_1 = fib_next
--- src/tests/tests_main.c
+++ src/tests/tests_main.c
@@ -20,29 +20,51 @@ void validate(char* case_name, bool case_stmt) {
     recomp_printf("Case %s %s\n", case_name, case_stmt ? "Passed" : "Failed!");
 }
 
+#define FIB_TABLE_SIZE 100000
+
 REPY_ON_INIT void REPY_Tests() {
     REPY_FN_SETUP;
     recomp_printf("REPY Tests Loaded\n");
-
-    u8 bytes_array[10] = { 1, 0, 1, 0, 1, 0, 1, 0, 1, 0 };
-    PyObjectHandle bytes_test = REPY_MemcpyToByteArray(bytes_array, 10, false);
-    REPY_FN_SET("bytes_test", bytes_test);
+        REPY_FN_EXEC_BLOCK(
+        time_start1,
+        "import time\n"
+        "nc_start_time = time.time()\n"
+    );
 
     REPY_FN_EXEC_BLOCK(
-        memcpy_test1,
-        "print(f'{len(bytes_test)=}')\n"
-        "print(f'{bytes_test=}')\n"
-        "for i in range(0, len(bytes_test)):\n"
-        "    bytes_test[i] += 1\n"
+        bad_fib1,
+        "count_holder = 0\n"
+        "def count_step():\n"
+        "    global count_holder\n"
+        "    count_holder += 1\n"
         "\n"
     );
-    REPY_MemcpyFromByteArray(bytes_array, 10, false, bytes_test);
-    recomp_printf("BytesArray: ");
-        
-    for (int i = 0; i < 10; i++) {
-        recomp_printf("%i", bytes_array[i]);
+    
+    u64* fib_table = recomp_alloc(sizeof(u64)* FIB_TABLE_SIZE);
+    PyObjectHandle fib_fn = REPY_FN_GET("count_step");
+
+    for (int i = 0; i < FIB_TABLE_SIZE; i++) {
+        REPY_Call(fib_fn, 0, 0);
+        fib_table[i] = REPY_FN_GET_U64("count_holder");
+        if (i % (FIB_TABLE_SIZE / 10) == 0) {
+            recomp_printf("=");
+        }
     }
-    recomp_printf("\n");
+    
+    REPY_FN_EXEC_BLOCK(
+        time_end1,
+        "fib_run_time = time.time() - nc_start_time\n"
+    );
+    recomp_printf("\n... ");
+    for (int i = 0; i < 5; i++) {
+        recomp_printf("%llu, ", fib_table[FIB_TABLE_SIZE - 6 + i]);
+    }
+
+    REPY_FN_EXEC_BLOCK(
+        time_print1,
+        "print(f'{fib_run_time=}')\n"
+    );
+    recomp_free(fib_table);
 
     // no_code_block_test();
     recomp_printf("REPY: Passed %i out of %i cases.\n", _test_cases_passed, _test_cases);
