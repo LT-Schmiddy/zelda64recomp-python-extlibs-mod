648dd56a60481c8d70d8d324e6e324533355d7f6
LT-Schmiddy <schmidalex1996@gmail.com>
2025-07-26 21:39:13 -0400

    pure lookup test

--- src/tests/tests_misc.c
+++ src/tests/tests_misc.c
@@ -376,4 +376,55 @@ REPY_ON_INIT void run_misc_tests() {
 
     REPY_FN_CLEANUP;
 }
+
+
+#define COUNT_TABLE_SIZE 100000
+REPY_ON_INIT void REPY_Lookup_And_Call_Speed_Test() {
+    REPY_FN_SETUP;
+    recomp_printf("REPY Tests Loaded\n");
+        REPY_FN_EXEC_CACHE(
+        time_start1,
+        "import time\n"
+        "nc_start_time = time.time()\n"
+    );
+
+    REPY_FN_EXEC_CACHE(
+        bad_fib1,
+        "count_holder = 0\n"
+        "def count_step():\n"
+        "    global count_holder\n"
+        "    count_holder += 1\n"
+        "\n"
+    );
+    
+    u64* count_table = recomp_alloc(sizeof(u64)* COUNT_TABLE_SIZE);
+    REPY_Handle count_fn = REPY_FN_GET("count_step");
+
+    for (int i = 0; i < COUNT_TABLE_SIZE; i++) {
+        REPY_Call(count_fn, 0, 0);
+        count_table[i] = REPY_FN_GET_U64("count_holder");
+        if (i % (COUNT_TABLE_SIZE / 10) == 0) {
+            recomp_printf("=");
+        }
+    }
+    
+    REPY_FN_EXEC_CACHE(
+        time_end1,
+        "fib_run_time = time.time() - nc_start_time\n"
+    );
+    recomp_printf("\n... ");
+    for (int i = 0; i < 5; i++) {
+        recomp_printf("%llu, ", count_table[COUNT_TABLE_SIZE - 6 + i]);
+    }
+
+    REPY_FN_EXEC_CACHE(
+        time_print1,
+        "print(f'{fib_run_time=}')\n"
+    );
+    recomp_free(count_table);
+
+    // no_code_block_test();
+    recomp_printf("REPY: Passed %i out of %i cases.\n", _test_cases_passed, _test_cases);
+    REPY_FN_CLEANUP;
+}
 */
