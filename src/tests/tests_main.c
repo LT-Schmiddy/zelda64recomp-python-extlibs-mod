f8c2d1d85171dadb8fa538b2d7342c7b72378e9c
LT-Schmiddy <schmidalex1996@gmail.com>
2026-02-02 14:13:31 -0500

    Test NRM has options for saving results and entering interactive shell

--- src/tests/tests_main.c
+++ src/tests/tests_main.c
@@ -1,6 +1,7 @@
 #include "modding.h"
 #include "global.h"
 #include "recomputils.h"
+#include "recompconfig.h"
 
 #include "repy_api.h"
 #include "test_utils.h"
@@ -606,8 +607,8 @@ REPY_ON_INIT void REPY_API_Tests() {
     REPY_Release(py_locals);
     recomp_printf("REPY: Passed %i out of %i cases.\n", _test_cases_passed, _test_cases);
 
-    
-    {
+
+    if (recomp_get_config_u32("save_case_count")) {
         REPY_FN_SETUP;
         REPY_FN_SET_S32("test_cases", _test_cases);
         REPY_FN_SET_S32("test_cases_passed", _test_cases_passed);
@@ -619,11 +620,12 @@ REPY_ON_INIT void REPY_API_Tests() {
         REPY_FN_CLEANUP;
     }
     
-
-    // recomp_printf("Starting Interavtive Shell. Call `exit()` to continue to game...\n");
-    // REPY_Handle code_module = REPY_ImportModule("code");
-    // REPY_CallAttrCStr(code_module, "interact", 0, 0);
-    // REPY_ClearError();
-    // REPY_Release(code_module);
+    if (recomp_get_config_u32("load_repl")) {
+        recomp_printf("Starting Interavtive Shell. Call `exit()` to continue to game...\n");
+        REPY_Handle code_module = REPY_ImportModule("code");
+        REPY_CallAttrCStr(code_module, "interact", 0, 0);
+        REPY_ClearError();
+        REPY_Release(code_module);
+    }
 }
 
