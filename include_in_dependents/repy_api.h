881344742a875a77130b7809f38ec72590bd2b61
LT-Schmiddy <schmidalex1996@gmail.com>
2026-02-24 23:36:27 -0500

    repy returns use an argument to ensure handle capture before

--- include_in_dependents/repy_api.h
+++ include_in_dependents/repy_api.h
@@ -630,10 +630,11 @@ REPY_PopInterpreter() \
  * 
  * The global scope is only released if the global and local scopes are the same.
  */
-#define REPY_FN_RETURN \
+#define REPY_FN_RETURN(retVal) \
+REPY_Handle __repy_retVal = retVal; \
 REPY_Release(REPY_FN_LOCAL_SCOPE); \
 REPY_PopInterpreter(); \
-return \
+return retVal \
 
 
 /**
@@ -1804,8 +1805,11 @@ REPY_FN_FOREACH_CACHE_CLEANUP_NOW(bytecode_identifier); break
  * 
  * @param bytecode_identifier The name for a static Python bytecode variable. Used to get the the `REPY_IteratorHelper` variable name.
  */
-#define REPY_FN_FOREACH_CACHE_RETURN(bytecode_identifier) \
-REPY_FN_FOREACH_CACHE_CLEANUP_NOW(bytecode_identifier); return
+#define REPY_FN_FOREACH_CACHE_RETURN(bytecode_identifier, retVal) \
+REPY_Handle __repy_retVal = retVal; \
+REPY_FN_FOREACH_CACHE_CLEANUP_NOW(bytecode_identifier); \
+REPY_FN_CLEANUP; \
+return
 
 /**
  * @brief Constructs a C style for loop, using cached Python code.
